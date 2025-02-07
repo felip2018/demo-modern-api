a07437708a87f17d3077699f3e40b0678c6412e2
felipe garzon <felipegarxon@hotmail.com>
2025-02-07 13:15:16 -0500

    get args from method with AOP

--- src/main/java/com/fgarzon/demomodernapi/aspects/LoggerAspect.java
+++ src/main/java/com/fgarzon/demomodernapi/aspects/LoggerAspect.java
@@ -1,7 +1,10 @@
 package com.fgarzon.demomodernapi.aspects;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Aspect
 @Component
@@ -9,8 +12,9 @@ public class LoggerAspect {
 
     // Antes de ejecutar un método de UserService
     @Before(value = "execution(* com.fgarzon.demomodernapi.service.IUserService.*(..))")
-    public void beforeUserService() {
-        System.out.println("-> [LOG] before method execution at UserService");
+    public void beforeUserService(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+        System.out.println("-> [LOG] before method execution at UserService with args: "+ Arrays.toString(args));
     }
 
     @After(value = "execution(* com.fgarzon.demomodernapi.service.IUserService.*(..))")
