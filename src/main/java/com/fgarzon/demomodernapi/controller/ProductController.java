614512f379af550abfb5410e008bd7088beddc05
felipe garzon <felipegarxon@hotmail.com>
2025-02-07 06:51:38 -0500

    add hateoas

--- src/main/java/com/fgarzon/demomodernapi/controller/ProductController.java
+++ src/main/java/com/fgarzon/demomodernapi/controller/ProductController.java
@@ -3,11 +3,16 @@ package com.fgarzon.demomodernapi.controller;
 import com.fgarzon.demomodernapi.entity.Product;
 import com.fgarzon.demomodernapi.service.impl.ProductServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.Link;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/v1/products")
@@ -17,7 +22,7 @@ public class ProductController {
     private ProductServiceImpl productService;
 
     @GetMapping("/{category}")
-    public ResponseEntity<List<Product>> getProductsByCategory(@PathVariable String category,
+    public ResponseEntity<CollectionModel<Product>> getProductsByCategory(@PathVariable String category,
                           @RequestHeader(value = "If-None-Match", required = false) String eTag) {
         System.out.println("eTag: " + eTag);
         List<Product> findedList = productService.getProductsByCategory(category);
@@ -28,7 +33,12 @@ public class ProductController {
             return ResponseEntity.status(HttpStatus.NOT_MODIFIED).build();
         }
 
-        return ResponseEntity.ok().eTag(currentTag).body(findedList);
+        Link link = linkTo(methodOn(ProductController.class).getProductsByCategory(category, eTag))
+                .withRel("productsByCategory");
+
+        CollectionModel<Product> result = CollectionModel.of(findedList, link);
+
+        return ResponseEntity.ok().eTag(currentTag).body(result);
     }
 
     private String generateETag(List<Product> findedList) {
